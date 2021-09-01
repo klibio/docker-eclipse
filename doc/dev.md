5f8abdb12c4f6036697b87ad0c7ab86ec98b86cd
PETER KIRSCHNER <peter@klib.io>
2021-09-01 09:06:21 +0200

    resolves #7

---
+++ doc/dev.md
@@ -0,0 +1,11 @@
+# Development
+
+## Java installation
+
+`jq '.'` is used for "pretty-print" formatting
+
+```bash
+#!/bin/bash
+curl -L 'https://api.adoptopenjdk.net/v2/info/releases/openjdk11?openjdk_impl=hotspot&os=linux&arch=x64&release=latest&type=jdk' \
+| jq '. | .binaries[0].binary_link }'
+ ```
--- resources/data/setup_01_java.sh
+++ resources/data/setup_01_java.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-set -eux && SCRIPT_DIR="$( cd $( dirname ${BASH_SOURCE[0]} ) >/dev/null 2>&1 && pwd )"
-
 # this script downloads and prepares the java runtime
-cd $SCRIPT_DIR
-JAVA_URL=https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.11%2B9/OpenJDK11U-jre_x64_linux_hotspot_11.0.11_9.tar.gz
-JAVA_ARCHIVE=$(echo "${JAVA_URL}" | sed "s/.*\///")
-mkdir $SCRIPT_DIR/jre
-curl -LfsSo ${JAVA_ARCHIVE} ${JAVA_URL} && tar -xvzf ${JAVA_ARCHIVE} --strip-components=1 -C $SCRIPT_DIR/jre
+set -eux && SCRIPT_DIR="$( cd $( dirname ${BASH_SOURCE[0]} ) >/dev/null 2>&1 && pwd )" && cd $SCRIPT_DIR
+
+# AdoptOpenJDK API - https://api.adoptopenjdk.net/README
+QUERY_URL="https://api.adoptopenjdk.net/v2/info/releases/openjdk11?openjdk_impl=hotspot&os=linux&arch=x64&release=latest&type=jdk"
+
+# jq - https://stedolan.github.io/jq/tutorial/
+JAVA_URL=$(curl -L ${QUERY_URL} | jq '. | .binaries[0].binary_link') && JAVA_URL=${JAVA_URL%\"} && JAVA_URL=${JAVA_URL#\"}
+JAVA_ARCHIVE=$(echo $JAVA_URL | sed "s/.*\///")
+JAVA_DIR=$SCRIPT_DIR/jdk
+mkdir -p $JAVA_DIR
+echo "downloading and extract into $JAVA_DIR from $JAVA_URL"
+curl -LfsSo $JAVA_ARCHIVE $JAVA_URL && tar -xvzf $JAVA_ARCHIVE --strip-components=1 -C $JAVA_DIR
 rm -rf $JAVA_ARCHIVE
